448bb19a883cb1f3af0c90c12f31aeda2b21393d
liquiid <liquiid727@outlook.com>
2024-04-30 00:39:47 +0800

    update component header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ title, subtitle }: { title: string, subtitle?: string }) => {
   return (
-    <div>
-      
-    </div>
+   <>
+    <h2 className='h2-bold text-dark-600'>{title}</h2>
+    {
+        subtitle && <p className='p-16-regular mt-4'>
+            {subtitle}
+        </p>
+    }
+   </>
   )
 }
 
