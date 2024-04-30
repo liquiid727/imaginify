c7762426471cdb6e3b683086d7e01ce41dc974ba
liquiid <liquiid727@outlook.com>
2024-04-30 18:04:34 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,11 @@
 import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
-
-// 中间件还需要改动
-const isProtectedRoute = createRouteMatcher([
-  '/', '/api/webhooks/clerk', '/api/webhooks/stripe'
-]);
+import { authMiddleware } from '@clerk/nextjs'
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk'],
+})
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
