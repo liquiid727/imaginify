c8695d9f34ed0e64090f5439f3979742f44585d7
liquiid <liquiid727@outlook.com>
2024-04-30 18:20:47 +0800

    update webhooks secret

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
 import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
-import { authMiddleware } from '@clerk/nextjs'
-
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk'],
-})
+
+export default clerkMiddleware()
 
